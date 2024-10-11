6150a6f4647340ba145779af0f249f008e478f6d
Amodh <amodh1257@gmail.com>
2024-10-11 19:58:11 -0400

    Added baseline framework to two files to start off with, including importing relevant libraries.

---
+++ pieces.py
@@ -0,0 +1,8 @@
+from rich import print
+from rich.traceback import install; install()
+from PIL import Image, ImageTk
+
+# TODO:
+# - Add pieces.
+# - Work piece logic.
+
---
+++ window.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import customtkinter as ctk
+from PIL import Image, ImageTk
+import os
+import sys
+
+from rich import print
+from rich.traceback import install; install()
+
+# TODO:
+# - Add window.
+# - Add icons.
+
